a3bdfa0648b71276355a57eb42c4ce704d17f142
Jaap Oosterbroek <jaap.oosterbroek@gmail.com>
2023-07-15 10:32:56 +0300

    version 0.2

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="srai-core",
+    packages=find_packages(),
+    version="0.1.0",
+    license='MIT',
+    package_data={},
+    python_requires=">=3.5",
+    install_requires=requirements,
+    author="Jaap Oosterbroek",
+    author_email="jaap.oosterbroek@southriverai.com",
+    description="A library core functions used in other SRAI libraries.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/southriverai/srai-core",
+    download_url='https://github.com/southriverai/srai-core/archive/v_01.tar.gz',
+    keywords=['SRAI', 'TOOLS'],
+    classifiers=[
+        'Development Status :: 3 - Alpha', 
+        'Intended Audience :: Developers',      # Define that your audience are developers
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',   # Again, pick a license
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+)
+
+
+
