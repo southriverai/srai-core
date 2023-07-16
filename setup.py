f7f44d8a4572a65d897dd3e21d064d08e9070a93
Jaap Oosterbroek <jaap.oosterbroek@gmail.com>
2023-07-16 11:58:33 +0300

    version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", "r") as f:
 setup(
     name="srai-core",
     packages=find_packages(),
-    version="0.4.1",
+    version="0.5.0",
     license="MIT",
     package_data={},
     python_requires=">=3.5",
